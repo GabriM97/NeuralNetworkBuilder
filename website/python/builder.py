8c2c439bccbdb1ed10d25ef479c055c374c54c40
Gabriele <marino.gabri97@gmail.com>
2019-08-13 00:31:27 +0200

    minor changes

--- website/python/builder.py
+++ website/python/builder.py
@@ -9,7 +9,6 @@ import pickle
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
-#from keras.optimizers import SGD
 
 
 def saveModelWeights(model, filename):
