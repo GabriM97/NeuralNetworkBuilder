5564938722cae9df4603309d0a0ddb5d93abc2fe
Gabriele <marino.gabri97@gmail.com>
2019-10-26 14:03:01 +0200

    minor changes on start script

--- utils/start.sh
+++ utils/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+gnome-terminal --tab --command="sudo supervisord -c /etc/supervisor/supervisord.conf"
+
 cd /PATH/TO/PHPMYADMIN
 gnome-terminal --tab --command="php -S localhost:7777"		#start PhpMyAdmin server
 
@@ -10,7 +12,6 @@ cd /PATH/TO/REPO/NeuralNetworkBuilder/nnb_website
 gnome-terminal --tab --command="npm run watch"  		#start watch (js and css files)
 
 #gnome-terminal --tab --command="sudo pkill supervisord"
-gnome-terminal --tab --command="sudo supervisord -c /etc/supervisor/supervisord.conf"
 gnome-terminal --tab --command="sudo supervisorctl"
 
 cd /PATH/TO/REPO/NeuralNetworkBuilder/nnb_website
